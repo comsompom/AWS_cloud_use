c43fefe0546cb2085e1919d707402a89eb9beb89
Oleg Bourdo <87123564+comsompom@users.noreply.github.com>
2025-04-24 16:21:30 +0300

    Create lambda_sample.py

---
+++ LocalStack/CDK/lambda/lambda_sample.py
@@ -0,0 +1,16 @@
+import boto3 
+
+
+def handler(event, context):
+    """
+    { "file_name": "sample_value" }
+    """
+    file_name = f'{event["file_name"]}.txt'
+    file_content = 'This is the content of the file.' 
+    
+    s3 = boto3.client('s3')
+    s3.put_object(Body=file_content, Bucket="lambda-sample-s3", Key=file_name)
+    return { 
+      'statusCode': 200, 
+      'body': 'File uploaded successfully.' 
+      }
