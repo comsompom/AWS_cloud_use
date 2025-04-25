e9a22ea51595cc9d520c224dbda04d6513049690
Oleg Bourdo <87123564+comsompom@users.noreply.github.com>
2025-04-25 12:07:51 +0300

    Update aws_stack.py

--- LocalStack/CDK/aws_stack.py
+++ LocalStack/CDK/aws_stack.py
@@ -4,7 +4,9 @@ from aws_cdk import (
     Stack,
     aws_sqs as sqs,
     aws_lambda as _lambda,
-    aws_s3 as s3
+    aws_lambda_event_sources as lambda_events,
+    aws_s3 as s3,
+    aws_dynamodb as _dynamo,
 )
 from constructs import Construct
 
@@ -13,6 +15,7 @@ class AwsStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # Create S3 bucket
         s3_bucket = s3.Bucket(
             self,
             "lambda-sample-s3",
@@ -37,25 +40,70 @@ class AwsStack(Stack):
             ]
         )
 
+        # Create API DynamoDB table
+        api_table = _dynamo.Table(
+            self, "demo_table",
+            partition_key=_dynamo.Attribute(
+                name="id",
+                type=_dynamo.AttributeType.STRING
+            )
+        )
+
+        # Create dead-letter queue
+        dlq = sqs.Queue(
+            self,
+            id="dead_letter_queue_id",
+            content_based_deduplication=True,
+            retention_period=Duration.days(7)
+        )
+        dead_letter_queue = sqs.DeadLetterQueue(
+            max_receive_count=5,
+            queue=dlq
+        )
+
+        # Create SQS between API and SQS Lambdas
         queue = sqs.Queue(
             self, "lambdaQueue",
+            dead_letter_queue=dead_letter_queue,
+            content_based_deduplication=True,
             visibility_timeout=Duration.seconds(300),
         )
 
-        lambda_function = _lambda.Function(self, "create-job",
+        # Create API Lambda
+        api_lambda = _lambda.Function(self, "api-lambda-job",
                                            runtime=_lambda.Runtime.PYTHON_3_10,
-                                           handler="lambda_sample.handler",
+                                           handler="api_lambda.handler",
                                            code=_lambda.Code.from_asset("./lambda"),
                                            environment=dict(
-                                               QUEUE_URL=queue.queue_url)
+                                               QUEUE_URL=queue.queue_url,
+                                               TABLE_NAME=api_table.table_name)
                                            )
-        lambda_function_url = lambda_function.add_function_url(
+        api_lambda_url = api_lambda.add_function_url(
             auth_type = _lambda.FunctionUrlAuthType.NONE,)
 
-        CfnOutput(self, "myFunctionUrlOutput", value=lambda_function_url.url)
-        CfnOutput(
-            self,
-            "Upload_Sqs_Queue_Url",
-            value=queue.queue_url,
-            description="One SQS Example",
-        )
+        # api_lambda.add_environment("TABLE_NAME", api_table.table_name)
+        api_table.grant_write_data(api_lambda)
+
+        # Create SQS Lambda
+        sqs_lambda = _lambda.Function(self, "sqs-lambda-job",
+                                      runtime=_lambda.Runtime.PYTHON_3_10,
+                                      handler="sqs_lambda.handler",
+                                      code=_lambda.Code.from_asset("./lambda"),
+                                      environment=dict(
+                                          QUEUE_URL=queue.queue_url,
+                                          TABLE_NAME=api_table.table_name)
+                                      )
+        sqs_lambda_url = sqs_lambda.add_function_url(
+            auth_type=_lambda.FunctionUrlAuthType.NONE, )
+        # sqs_lambda.add_environment("TABLE_NAME", api_table.table_name)
+        api_table.grant_write_data(sqs_lambda)
+
+        # This binds the lambda to the SQS Queue
+        invoke_event_source = lambda_events.SqsEventSource(queue)
+        sqs_lambda.add_event_source(invoke_event_source)
+
+        CfnOutput(self, "API_Lambda_URL", value=api_lambda_url.url)
+        CfnOutput(self, "SQS_Lambda_URL", value=sqs_lambda_url.url)
+        CfnOutput(self, "Upload_Sqs_Queue_Url", value=queue.queue_url,
+                  description="One SQS Example")
+        CfnOutput(self, "DynamoDB_table_name", value=api_table.table_name)
