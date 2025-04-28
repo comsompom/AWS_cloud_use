55cc5a4f298618b4ecaf0bffdf89bdb81d260d64
Oleg Bourdo <87123564+comsompom@users.noreply.github.com>
2025-04-28 13:39:22 +0300

    Create sqs_lambda.ts

---
+++ LocalStack/CDK_TS/sqs_lambda/sqs_lambda.ts
@@ -0,0 +1,10 @@
+// Define the shape of the input event
+type UserMsgEvent = {
+    id: string;
+    message: string;
+    image: string;
+}
+
+export const handler = async (event: UserMsgEvent): Promise<string> => {
+    return event.message;
+};
